39cbe3e6527384d38ac098a525779b959f337869
craftidev <ivann.codev@gmail.com>
2024-09-23 23:27:32 +0200

    first rework, still need confirmation

--- internal/db/models.go
+++ internal/db/models.go
@@ -1,42 +1,58 @@
 package db
 
 import (
-    "time"
+	"fmt"
+	"time"
 )
 
+// List of models: Client, Session, Amount, Expense
+// (Not its own DB table: Amount)
+
+// Client
 type Client struct {
-    ID      int
-    Name    string
-    Address string
-    Phone   string
-    Email   string
+    ID   int
+    Name string
 }
 
+func (c Client) String() string {
+    return fmt.Sprintf(c.Name)
+}
+
+
+// Session
 type Session struct {
     ID       int
     ClientID int
+    Name     string
+    Address  string
     StartAt  time.Time
     EndAt    time.Time
 }
 
-type Category struct {
-    ID          int
-    Name        string
-    Description string
+func (s Session) String() string {
+    return fmt.Sprintf(s.Name)
 }
 
-type Reason struct {
-    ID          int
-    CategoryID  int
-    Name        string
-    Description string
+// Amount
+type Amount struct {
+    Value    float64
+    Currency string
+}
+
+func (a Amount) String() string {
+    return fmt.Sprintf("%.2f %s", a.Value, a.Currency)
 }
 
 type Expense struct {
-    ID          int
-    SessionID   int
-    ReasonID    int
-    Description string
-    Amount      float64
-    DateTime    time.Time
+    ID         int
+    SessionID  int
+    Type       string
+    Amount     Amount
+    Location   string
+    DateTime   time.Time
+    ReceiptURL string
+}
+
+func (e Expense) String() string {
+    return fmt.Sprintf("%s (%v)", e.Amount, e.Type)
 }
