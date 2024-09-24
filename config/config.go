17e0ffb700e80ac284ee0bcb9705b9c5d311c7cd
craftidev <ivann.codev@gmail.com>
2024-09-24 18:46:23 +0200

    path

--- config/config.go
+++ config/config.go
@@ -1,3 +1,6 @@
 package config
 
-const DefaultReceiptURL = "/assets/receipts/default.jpg"
+const (
+    DefaultReceiptURL = "/assets/receipts/default.jpg"
+    Path = "/home/ivanncodev/dev/projects/ExpenseFlow"
+)
