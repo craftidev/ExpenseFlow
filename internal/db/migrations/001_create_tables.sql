b6196971f68792dd3833bc575af6e70ee7ec7c61
craftidev <ivann.codev@gmail.com>
2024-09-25 20:57:15 +0200

    sqlite doesn't let us ADD CONSTRAINT after the creation

--- internal/db/migrations/001_create_tables.sql
+++ internal/db/migrations/001_create_tables.sql
@@ -1,8 +1,9 @@
--- Limit TEXT size on some column
-
 CREATE TABLE IF NOT EXISTS clients (
     id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    name            TEXT     NOT NULL UNIQUE
+    name            TEXT     NOT NULL UNIQUE,
+
+    CONSTRAINT ck_non_empty_fields               CHECK (LENGTH(name) > 0),
+    CONSTRAINT ck_normal_size_name_100           CHECK (LENGTH(name) <= 100)
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
@@ -13,14 +14,32 @@ CREATE TABLE IF NOT EXISTS sessions (
     start_at        TEXT     NOT NULL,
     end_at          TEXT     NOT NULL,
 
-    FOREIGN KEY (client_id) REFERENCES clients(id)
+    FOREIGN KEY (client_id)  REFERENCES clients(id)
     ON UPDATE CASCADE
-    ON DELETE RESTRICT
+    ON DELETE RESTRICT,
+
+    CONSTRAINT ck_non_empty_fields               CHECK (
+        LENGTH(name) > 0 AND
+        LENGTH(address) > 0 AND
+        LENGTH(start_at) > 0 AND
+        LENGTH(end_at) > 0
+    ),
+    CONSTRAINT ck_normal_size_name_address_100   CHECK (
+        LENGTH(address) <= 100 AND
+        LENGTH(name) <= 100
+    ),
+    CONSTRAINT ck_normal_size_start_at_end_at_19 CHECK (
+        LENGTH(start_at) == 19 AND
+        LENGTH(end_at) == 19
+    )
 );
 
 CREATE TABLE IF NOT EXISTS expense_types (
     id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    name            TEXT     NOT NULL UNIQUE
+    name            TEXT     NOT NULL UNIQUE,
+
+    CONSTRAINT ck_non_empty_name                 CHECK (LENGTH(name) > 0),
+    CONSTRAINT ck_normal_size_name_50            CHECK (LENGTH(name) <= 50)
 );
 
 CREATE TABLE IF NOT EXISTS expenses (
@@ -35,9 +54,21 @@ CREATE TABLE IF NOT EXISTS expenses (
 
     FOREIGN KEY (session_id) REFERENCES sessions(id)
     ON UPDATE CASCADE
-    ON DELETE RESTRICT
+    ON DELETE RESTRICT,
 
-    FOREIGN KEY (type_id) REFERENCES expense_types(id)
+    FOREIGN KEY (type_id)    REFERENCES expense_types(id)
     ON UPDATE CASCADE
-    ON DELETE RESTRICT
+    ON DELETE RESTRICT,
+
+    CONSTRAINT ck_positive_amount                CHECK (amount_value > 0),
+    CONSTRAINT ck_non_empty_fields               CHECK (
+        LENGTH(amount_currency)    >  0 AND
+        LENGTH(location)           >  0 AND
+        LENGTH(datetime)           >  0 AND
+        LENGTH(receipt_url)        >  0
+    ),
+    CONSTRAINT ck_normal_size_currency_10        CHECK (LENGTH(amount_currency) <= 10),
+    CONSTRAINT ck_normal_size_location_100       CHECK (LENGTH(location) <= 100),
+    CONSTRAINT ck_normal_size_datetime_19        CHECK (LENGTH(datetime) == 19),
+    CONSTRAINT ck_normal_size_receipt_url_150    CHECK (LENGTH(receipt_url) <= 150)
 );
