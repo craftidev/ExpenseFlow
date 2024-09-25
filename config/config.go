6f7f2cfde3bfecd891a0de05c2e5c3e282060a33
craftidev <ivann.codev@gmail.com>
2024-09-25 21:28:47 +0200

    add db and migration path

--- config/config.go
+++ config/config.go
@@ -3,4 +3,6 @@ package config
 const (
     DefaultReceiptURL = "/assets/receipts/default.jpg"
     Path = "/home/ivanncodev/dev/projects/ExpenseFlow"
+    DBPath = Path + "/internal/db/expenseflow.db"
+    MigrationsDirPath = Path + "/internal/db/migrations"
 )
