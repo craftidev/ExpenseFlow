f66fdc4cd94338ab4bb96e6f52b2d3a8bc38aecc
craftidev <ivann.codev@gmail.com>
2024-09-25 21:57:29 +0200

    setup logging file; init/connect/close DB

--- cmd/expenseflow/main.go
+++ cmd/expenseflow/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+    "log"
+    "os"
+    "github.com/craftidev/expenseflow/internal/db"
+)
+
+
+func setupLogging() {
+    logFile, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+    if err != nil {
+        log.Fatalf("Failed to open log file: %v", err)
+    }
+
+    log.SetOutput(logFile)
+
+    log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
+
+func main() {
+    setupLogging()
+
+    database := db.ConnectDB()
+    defer db.CloseDB(database)
+
+    db.InitDB(database)
+
+    // TODO: Implement CRUD
+    log.Println("ExpenseFlow DB connection established")
+}
