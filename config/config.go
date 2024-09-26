daa56a39109c017558590ce4ece36975b4224df5
craftidev <ivann.codev@gmail.com>
2024-09-26 14:26:22 +0200

    add custom float max (used for Amount.Value and don't go into the real MaxFloat64 shenanigans)

--- config/config.go
+++ config/config.go
@@ -5,4 +5,5 @@ const (
     Path = "/home/ivanncodev/dev/projects/ExpenseFlow"
     DBPath = Path + "/internal/db/expenseflow.db"
     MigrationsDirPath = Path + "/internal/db/migrations"
+    MaxFloat = 1_000_000_000.0
 )
