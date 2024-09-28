6a581b47a0742b9d1f04b3214866af5a108b3ed9
craftidev <ivann.codev@gmail.com>
2024-09-28 14:45:19 +0200

    add standard migration for business: ORISHA G8

---
+++ internal/db/migrations/standard_models/orisha_g8.sql
@@ -0,0 +1,11 @@
+-- TODO correct taxes
+INSERT INTO expense_types(name, taxe_rate) VALUES ("PEAGE", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("PARKING", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("AF/SNCF", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("LOC VOITURE", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("HOTEL", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("REPAS MIDI", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("REPAS SOIR", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("GASOIL", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("ACHAT Div", 0);
+INSERT INTO expense_types(name, taxe_rate) VALUES ("Boissons alcoolisées", 20);
