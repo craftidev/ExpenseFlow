2ab0f7c42afa56feed94208ef2694b677ca3f6ac
craftidev <ivann.codev@gmail.com>
2024-09-29 13:32:53 +0200

    DB schema rework after end user feedback

--- internal/db/migrations/001_create_tables.sql
+++ internal/db/migrations/001_create_tables.sql
@@ -1,79 +1,85 @@
 CREATE TABLE IF NOT EXISTS clients (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    name            TEXT     NOT NULL UNIQUE,
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT    NOT NULL UNIQUE,
 
-    CONSTRAINT ck_non_empty_fields               CHECK (LENGTH(name) > 0),
-    CONSTRAINT ck_normal_size_name_100           CHECK (LENGTH(name) <= 100)
+    CONSTRAINT ck_non_empty_fields     CHECK (LENGTH(name) > 0),
+    CONSTRAINT ck_normal_size_name_100 CHECK (LENGTH(name) <= 100)
 );
 
 CREATE TABLE IF NOT EXISTS sessions (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    client_id       INTEGER  NOT NULL,
-    name            TEXT     NOT NULL UNIQUE,
-    address         TEXT     NOT NULL,
-    start_at        TEXT     NOT NULL,
-    end_at          TEXT     NOT NULL,
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_id           INTEGER  NOT NULL,
+    location            TEXT     NOT NULL,
+    trip_start_location TEXT         NULL,
+    trip_end_location   TEXT         NULL,
+    start_at_date       TEXT         NULL,
+    end_at_date         TEXT         NULL,
 
-    FOREIGN KEY (client_id)  REFERENCES clients(id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
+    FOREIGN KEY (client_id)  REFERENCES clients(id),
 
-    CONSTRAINT ck_non_empty_fields               CHECK (
-        LENGTH(name) > 0 AND
-        LENGTH(address) > 0 AND
-        LENGTH(start_at) > 0 AND
-        LENGTH(end_at) > 0
+    CONSTRAINT ck_non_empty_fields                         CHECK (
+        LENGTH(location)            > 0 AND
+        LENGTH(trip_start_location) > 0 AND
+        LENGTH(trip_end_location)   > 0 AND
     ),
-    CONSTRAINT ck_normal_size_name_address_100   CHECK (
-        LENGTH(address) <= 100 AND
-        LENGTH(name) <= 100
+    CONSTRAINT ck_normal_size_name_locations_100           CHECK (
+        LENGTH(location)            <= 100 AND
+        LENGTH(trip_start_location) <= 100 AND
+        LENGTH(trip_end_location)   <= 100
     ),
-    CONSTRAINT ck_normal_size_start_at_end_at_19 CHECK (
-        LENGTH(start_at) == 19 AND
-        LENGTH(end_at) == 19
+    CONSTRAINT ck_normal_size_start_at_date_end_at_date_19 CHECK (
+        start_at_date == NULL OR LENGTH(start_at_date) == 10 AND
+        end_at_date   == NULL OR LENGTH(end_at_date)   == 10
     )
 );
 
-CREATE TABLE IF NOT EXISTS expense_types (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    name            TEXT     NOT NULL UNIQUE,
-    taxe_rate       REAL     NOT NULL, -- TODO new entity for taxes
+CREATE TABLE IF NOT EXISTS car_trips(
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id  INTEGER     NULL,
+    distance_km REAL    NOT NULL,
+    date        TEXT    NOT NULL UNIQUE,
 
-    CONSTRAINT ck_non_empty_name                 CHECK (LENGTH(name) > 0),
-    CONSTRAINT ck_normal_size_name_50            CHECK (LENGTH(name) <= 50),
-    CONSTRAINT ck_positive_taxe_rate             CHECK (taxe_rate >= 0),
-    CONSTRAINT ck_normal_size_taxe_rate_60       CHECK (taxe_rate <= 60)
+    FOREIGN KEY (session_id) REFERENCES sessions(id),
+
+    CONSTRAINT ck_normal_size_date_10  CHECK (LENGTH(date) == 10),
+    CONSTRAINT ck_positive_distance_km CHECK (distance_km  > 0)
+);
+
+CREATE TABLE IF NOT EXISTS expense_types (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT    NOT NULL UNIQUE,
+
+    CONSTRAINT ck_non_empty_name      CHECK (LENGTH(name) > 0),
+    CONSTRAINT ck_normal_size_name_50 CHECK (LENGTH(name) <= 50)
 );
 
 CREATE TABLE IF NOT EXISTS expenses (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
-    session_id      INTEGER      NULL, -- NULLABLE FK
-    type_id         INTEGER  NOT NULL,
-    amount_value    REAL     NOT NULL, -- mapped in Amount.value
-    amount_currency TEXT     NOT NULL, -- mapped in Amount.currency
-    location        TEXT     NOT NULL,
-    datetime        TEXT     NOT NULL,
-    receipt_url     TEXT     NOT NULL,
-    notes           TEXT     NOT NULL,
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id  INTEGER     NULL,
+    type_id     INTEGER NOT NULL,
+    currency    TEXT    NOT NULL,
+    receipt_url TEXT        NULL,
+    notes       TEXT        NULL,
 
-    FOREIGN KEY (session_id) REFERENCES sessions(id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
+    FOREIGN KEY (session_id) REFERENCES sessions(id),
+    FOREIGN KEY (type_id)    REFERENCES expense_types(id),
 
-    FOREIGN KEY (type_id)    REFERENCES expense_types(id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
+    CONSTRAINT ck_non_empty_currency         CHECK (LENGTH(currency)    >  0),
+    CONSTRAINT ck_normal_size_currency_10    CHECK (LENGTH(currency)    <= 10),
+    CONSTRAINT ck_normal_size_location_100   CHECK (LENGTH(location)    <= 100),
+    CONSTRAINT ck_normal_size_receipt_url_50 CHECK (LENGTH(receipt_url) <= 50),
+    CONSTRAINT ck_normal_notes_150           CHECK (LENGTH(notes)       <= 150)
+);
 
-    CONSTRAINT ck_positive_amount                CHECK (amount_value > 0),
-    CONSTRAINT ck_non_empty_fields               CHECK (
-        LENGTH(amount_currency)    >  0 AND
-        LENGTH(location)           >  0 AND
-        LENGTH(datetime)           >  0 AND
-        LENGTH(receipt_url)        >  0
-    ),
-    CONSTRAINT ck_normal_size_currency_10        CHECK (LENGTH(amount_currency) <= 10),
-    CONSTRAINT ck_normal_size_location_100       CHECK (LENGTH(location) <= 100),
-    CONSTRAINT ck_normal_size_datetime_19        CHECK (LENGTH(datetime) == 19),
-    CONSTRAINT ck_normal_size_receipt_url_150    CHECK (LENGTH(receipt_url) <= 150), -- TODO reduce
-    CONSTRAINT ck_normal_notes_150               CHECK (LENGTH(notes) <= 150)
+CREATE TABLE IF NOT EXISTS line_items (
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    expense_id INTEGER NOT NULL,
+    taxe_rate  REAL    NOT NULL,
+    total      REAL    NOT NULL,
+
+    FOREIGN KEY (expense_id) REFERENCES expenses(id),
+
+    CONSTRAINT ck_positive_total           CHECK (taxe_rate > 0),
+    CONSTRAINT ck_positive_taxe_rate       CHECK (taxe_rate >= 0),
+    CONSTRAINT ck_normal_size_taxe_rate_60 CHECK (taxe_rate <= 60)
 );
