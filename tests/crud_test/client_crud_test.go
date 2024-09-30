8cc5fd7fcb70d6c84df073704048d461f0229319
craftidev <ivann.codev@gmail.com>
2024-09-30 18:51:10 +0200

    complete client crud test with Update and Delete

--- tests/crud_test/client_crud_test.go
+++ tests/crud_test/client_crud_test.go
@@ -3,6 +3,7 @@ package crud_test
 import (
 	"database/sql"
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/craftidev/expenseflow/internal/db"
@@ -59,15 +60,84 @@ func TestCreateClient(t *testing.T) {
     }
 }
 
-// For GetClientByID()
-    // Test case with fetching valid client
-    // fetchedClient, err := crud.GetClientByID(DatabaseTest, id)
-    // if err!= nil {
-    //     t.Errorf("expected no error, got: %v", err)
-    // }
-    // if fetchedClient == nil {
-    //     t.Error("expected valid client, got nil")
-    // }
-    // if fetchedClient.ID!= client.ID || fetchedClient.Name!= client.Name {
-    //     t.Errorf("expected client %+v, got %+v", client, fetchedClient)
-    // }
+func TestGetClientByID(t *testing.T) {
+    // Can't run this test alone because use TestCreateClient valid entry
+    // Valid fetch
+    fetchedClient, err := crud.GetClientByID(DatabaseTest, 1)
+    if err != nil {
+        t.Errorf("expected no error, got: %v", err)
+    }
+    if fetchedClient.Valid() != nil {
+        t.Error("expected valid client, got invalid")
+    }
+
+    // Invalid fetch
+    _, err = crud.GetClientByID(DatabaseTest, 9999)
+    if err == nil {
+        t.Error("expected error with none existant id, got no error")
+    }
+}
+
+func TestUpdateClient(t *testing.T) {
+    // Can't run this test alone because use TestCreateClient valid entry
+
+    // Valid update
+    client := db.Client{
+        ID:   1,
+        Name: "John Doe Updated",
+    }
+    err := crud.UpdateClient(DatabaseTest, client)
+    if err!= nil {
+        t.Errorf("expected no error, got: %v", err)
+    }
+
+    // Invalid update (zero ID)
+    client.ID = 0
+    err = crud.UpdateClient(DatabaseTest, client)
+    if err == nil {
+        t.Error("expected error for invalid client with zero ID")
+    }
+
+	var invalidClients [4]db.Client
+	for i := 0; i < len(invalidClients); i++ {
+		invalidClients[i] = client
+        invalidClients[i].Name += strconv.Itoa(i)
+	}
+
+	invalidClients[0].ID = -1
+	invalidClients[1].ID = 0
+	invalidClients[2].ID = 9999
+	invalidClients[3].Name = "John Doe Updated"
+
+	for i, invalidClient := range invalidClients {
+		err = crud.UpdateClient(DatabaseTest, invalidClient)
+		if err == nil {
+			t.Errorf(
+                "expected error, got valid client on UpdateClient method " +
+                "test number: %d", i,
+            )
+		}
+	}
+}
+
+func TestDeleteClientByID(t *testing.T) {
+    // Can't run this test alone because use TestCreateClient valid entry
+
+    err := crud.DeleteClientByID(DatabaseTest, 1)
+    if err != nil {
+        t.Errorf("expected no error, got: %v", err)
+    }
+
+    err = crud.DeleteClientByID(DatabaseTest, 1)
+    if err == nil {
+        t.Errorf("expected an error, client doesn't exist, got: %v", err)
+    }
+    err = crud.DeleteClientByID(DatabaseTest, 0)
+    if err == nil {
+        t.Errorf("expected an error, id zero-value, got: %v", err)
+    }
+    err = crud.DeleteClientByID(DatabaseTest, -1)
+    if err == nil {
+        t.Errorf("expected an error, id negative, got: %v", err)
+    }
+}
