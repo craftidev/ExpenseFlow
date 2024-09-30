ce2efd2260bd321f074eaa0a7941fd5e30745f76
craftidev <ivann.codev@gmail.com>
2024-09-30 20:06:32 +0200

    Helper function, confusing vscode a little but too practical to not use with workaround

--- tests/test_setup.go
+++ tests/test_setup.go
@@ -3,10 +3,11 @@ package tests
 import (
 	"database/sql"
 	"log"
+	"runtime"
+	"testing"
 
 	"github.com/craftidev/expenseflow/internal/db"
 )
-
 
 var SingletonDatabaseTest *sql.DB
 
@@ -34,5 +35,31 @@ func TeardownTestDatabase() {
 			log.Fatalf("Failed to close database: %v", err)
 		}
 		SingletonDatabaseTest = nil
+	}
+}
+
+func InitializeSliceOfValidAny[T any](size int, valid T) []T {
+	slice := make([]T, size)
+	for i := range slice {
+		slice[i] = valid
+	}
+	return slice
+}
+
+// Provoke vscode to get confused
+// when a test fail because of entities validated in that function,
+// the auto-opening feature try to opent this file
+// with the path of the test file and open an inexistant file
+// workaround -> return real calling location in t.Error
+func ValidateInvalidEntities[T any](t *testing.T, entities []T, validator func(T) error) {
+	for i, entity := range entities {
+		err := validator(entity)
+		if err == nil {
+            _, file, line, _ := runtime.Caller(1)
+			t.Errorf(
+                "expected error, got valid entity on test number: %d @ %v:%v",
+                i, file, line,
+            )
+		}
 	}
 }
