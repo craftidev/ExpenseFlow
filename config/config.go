586cb87bdbb4ce3660e1841aa222f3766f2d9759
craftidev <ivann.codev@gmail.com>
2024-09-30 17:30:13 +0200

    use runtime to find AppPath more reliably. Add path for receipts tests

--- config/config.go
+++ config/config.go
@@ -1,25 +1,26 @@
 package config
 
 import (
-	"log"
-	"os"
 	"path/filepath"
+	"runtime"
 )
 
+
 func getAppPath() string {
-    path, err := os.Getwd()
-    if err != nil {
-        log.Fatalf("[fatal] couldn't find working directory: %v", err)
-    }
-    return path
+    // Get Dir of this particular file and go up the tree
+    _, b, _, _ := runtime.Caller(0)
+    basePath := filepath.Dir(filepath.Join(filepath.Dir(b)))
+    return basePath
 }
 
 var (
     Path = getAppPath()
     DBPath = filepath.Join(Path, "internal", "db", "expenseflow.db")
     ReceiptsDir = filepath.Join(Path, "assets", "receipts")
-    MigrationsDirPath = filepath.Join(Path,"internal", "db", "migrations")
+    ReceiptsDirTest = filepath.Join(Path, "tests", "assets", "receipts")
+    MigrationsDirPath = filepath.Join(Path, "internal", "db", "migrations")
 )
+
 const (
     MaxFloat = 1_000_000_000.0
 )
