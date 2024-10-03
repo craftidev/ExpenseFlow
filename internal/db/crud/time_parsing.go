d738fff6ecc0117cd69feecab66fde8de54aea87
craftidev <ivann.codev@gmail.com>
2024-10-03 17:54:11 +0200

    correct the parsing layout

--- internal/db/crud/time_parsing.go
+++ internal/db/crud/time_parsing.go
@@ -2,7 +2,6 @@ package crud
 
 import (
 	"database/sql"
-	"strings"
 	"time"
 
 	"github.com/craftidev/expenseflow/internal/db"
@@ -12,14 +11,11 @@ import (
 
 func ParsingStrToTime(timestamp string) (time.Time, error) {
 	// The full timestamp string looks like this with time.Time
-    // "2024-10-03 14:28:59.052881354 +0200 CEST m=+0.004487003"
-	parts := strings.Split(timestamp, " m=")
-	cleanedTimestamp := parts[0]
-
-    layout := "2006-01-02 15:04:05.999999999 -0700 MST"
-	parsedTime, err := time.Parse(layout, cleanedTimestamp)
+    // "2024-10-03 16:57:00.013887899+02:00"
+    layout := "2006-01-02 15:04:05.999999999-07:00"
+	parsedTime, err := time.Parse(layout, timestamp)
 	if err != nil {
-		return time.Time{}, utils.LogError("Error parsing time: %v", err)
+		return time.Time{}, utils.LogError("error parsing time: %v", err)
 	}
 
 	return parsedTime, nil
